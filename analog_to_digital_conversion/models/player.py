80a41bed06dcb6aaae1dca5aba49a2c51ab8bbc3
236659 Mikołaj Stępniak <236659@edu.p.lodz.pl>
2022-05-27 16:26:06 +0200

    AC/CA: player update

--- analog_to_digital_conversion/models/player.py
+++ analog_to_digital_conversion/models/player.py
@@ -1,2 +1,22 @@
+import numpy as np
+import soundcard as sc
+from scipy.io.wavfile import read
+
+
 class Player:
-    ...
+    def __init__(self, sampling):
+        self.sampling = sampling
+        self.audio = sc.default_speaker()
+        self.channels = None
+
+    def play_recorded(self, data: np.ndarray):
+        self.audio.play(data, self.sampling, self.channels)
+
+    def play_saved(self, filename: str):
+        data = read(filename)
+        sampling = data[0]
+        data = np.float64(data[1] / np.max(abs(data[1])))
+        channels = []
+        for i in range(len(data[0])):
+            channels.append(i)
+        self.audio.play(data, sampling, channels)
