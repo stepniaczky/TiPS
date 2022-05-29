d0d4ee1f67bdc351304be26609964cf50b90e968
236659 Mikołaj Stępniak <236659@edu.p.lodz.pl>
2022-05-29 12:04:53 +0200

    AC/CA: main update

--- analog_to_digital_conversion/main.py
+++ analog_to_digital_conversion/main.py
@@ -1,40 +1,77 @@
-import keyboard
+import logging
 from models.player import Player
 from models.recorder import Recorder
+from time import sleep
+
+
+def int_choice(msg, cond=None):
+    while True:
+        try:
+            n = int(input(msg))
+            if cond is not None:
+                if n not in cond:
+                    raise ValueError
+            return n
+        except ValueError:
+            print("Try again!")
 
 
 def main():
-    sampling = abs(int(input("Frequency of sampling [Hz]: ")))
-    quantization = int(input("Level of quantization [8, 16, 32]: "))
+    sampling = abs(int_choice("Frequency of sampling [Hz]: "))
+    quantization = int_choice("Level of quantization [8, 16, 32]: ", [8, 16, 32])
+    print()
 
-    print("Press 'r' to start recording",
-          "Press 'm' to stop recording"
-          "Press 's' to save recorded audio",
-          "Press 'p' to play recorded audio",
-          "Press 'z' to play saved audio",
-          "Press 'q' to quit the program", sep="\n")
+    menu = ["Type 'r' to start recording", "Type 's' to save recorded audio",
+            "Type 'p' to play recorded audio", "Type 'z' to play saved audio",
+            "Type 'q' to quit the program", "Type 'h' to display all options"]
+
+    for line in menu:
+        print(line)
 
     recorder = Recorder(sampling, quantization)
     player = Player(sampling)
+    logging.basicConfig(format='%(asctime)s : %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
+    msg = ''
 
     while True:
-        if keyboard.is_pressed('r'):
-            while True:
-                if keyboard.is_pressed('m'):
-                    break
-                recorder.record(1)
+        opt = input("\nOption: ")
+        match opt:
+            case 'r':
+                recorder.clear_recorded_audio()
+                time = int_choice("Set recording time [s]: ")
+                logging.info("Recording is starting in: ")
+                sleep(0.1)
+                i = 3
+                while i > 0:
+                    print(f"{i}...")
+                    sleep(1)
+                    i -= 1
+                recorder.record(time)
+                logging.info("Recording is stopped.")
 
-        if keyboard.is_pressed('s'):
-            recorder.save()
+            case 's':
+                msg = recorder.save("audio.wav")
 
-        if keyboard.is_pressed('p'):
-            player.play_recorded()
+            case 'p':
+                msg = player.play_recorded(recorder.rec_audio)
 
-        if keyboard.is_pressed('z'):
-            player.play_saved()
+            case 'z':
+                msg = player.play_saved("audio.wav")
 
-        if keyboard.is_pressed('q'):
-            break
+            case 'q':
+                logging.info("Exiting the program.")
+                break
+
+            case 'h':
+                for line in menu:
+                    print(line)
+
+            case _:
+                logging.info("Unknown option.")
+
+        if opt in ['s', 'p', 'z']:
+            logging.info(msg)
+        sleep(0.1)
 
 
 if __name__ == '__main__':
